a70dd6febe7c08fd40e49481a3d34a78d6520a46
Nelson Peña <nepv2308@live.com>
2026-06-14 11:13:35 -0600

    feat: initialize express server and implement user CRUD routes

---
+++ SRC/routes/user.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => {
+    //Buscar en la base de Datos 
+    console.log("Alguien consulto el Endpoint")
+    res.status(200).json({ message: "EndPoint de obtener funcionando 👌" });
+});
+
+userRouter.post("/create", (req, res) => {
+    const { name, age } = req.body
+    if (!name || !age) {
+        return res.status(400).json({ message: "Debe enviar nombre y edad" })
+    }
+    res.status(201).json({ message: `el usuario ${name} de ${age} se ha creado` });
+});
+userRouter.put("/update/:id", (req, res) => {
+    const { id } = req.params
+    const { name, age } = req.body
+    if (!name || !age) {
+        return res.status(400).json({ message: "Debe enviar nombre y edad" })
+    }
+    res.status(200).json({ message: `el usuario ${name} de ${age} se ha actualizado` });
+})
+userRouter.delete("/delete/:id", (req, res) => {
+    const { id } = req.params
+    res.status(200).json({ message: `el usuario con id ${id} se ha eliminado` });
+})
+
+userRouter.get("/test", (req, res) => {
+    res.status(200).json({ message: "todo chivo, no te estreses 👌" });
+});
+
+export default userRouter
